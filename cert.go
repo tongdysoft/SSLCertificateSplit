b35ef4feab3479d3cdabbfdbf30a1de4635a75fd
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2024-03-26 20:42:39 +0800

    搜索客户端证书与和其他证书的层级

--- cert.go
+++ cert.go
@@ -5,7 +5,9 @@ import (
 	"encoding/pem"
 	"fmt"
 	"log"
+	"net/url"
 	"os"
+	"strings"
 )
 
 var (
@@ -13,6 +15,7 @@ var (
 	blocks  []*pem.Block        = []*pem.Block{}
 	x509s   []*x509.Certificate = []*x509.Certificate{}
 	lineStr string              = "----------"
+	order   []int               = []int{}
 )
 
 func loadCertFile() bool {
@@ -25,6 +28,7 @@ func loadCertFile() bool {
 	splitPEMCerts(certPEM)
 	var numOK [2]int8 = parseCertificates()
 	log.Println("文件包含证书数:", numOK[0]+numOK[1], " 有效证书数:", numOK[0], " 无效证书数:", numOK[1])
+	certificateOrder()
 	viewCertInfo()
 	return true
 }
@@ -72,36 +76,103 @@ func parseCertificates() [2]int8 {
 	return r
 }
 
-func viewCertInfo() bool {
-	if len(x509s) == 0 {
-		log.Println("错误：没有证书可供查看。")
-		return false
+func uniqueStrings(strings []string) []string {
+	// 创建一个映射来记录每个字符串出现的次数。
+	countMap := make(map[string]int)
+	for _, str := range strings {
+		countMap[str]++
 	}
+	// 创建一个空切片来存储没有重复的字符串。
+	var unique []string
+	for str, count := range countMap {
+		// 如果某个字符串的出现次数为1，那么它就是没有重复的，加入到结果切片中。
+		if count == 1 {
+			unique = append(unique, str)
+		}
+	}
+	return unique
+}
+
+func certificateOrder() {
+	var newx509s []int = []int{}
+	var endSub = ""
+	var allCNs []string = make([]string, len(x509s)*2)
 	for i, cert := range x509s {
-		fmt.Println("\n", lineStr, "证书", i+1, "/", len(x509s), lineStr)
-		fmt.Printf("版本: %d\n", cert.Version)
-		fmt.Printf("序列号: %d\n", cert.SerialNumber)
-		fmt.Printf("签名算法: %s\n", cert.SignatureAlgorithm)
-		fmt.Printf("颁发者: %s\n", cert.Issuer)
-		fmt.Printf("使用者: %s\n", cert.Subject)
-		fmt.Printf("有效期开始时间: %s\n", cert.NotBefore)
-		fmt.Printf("有效期结束时间: %s\n", cert.NotAfter)
-		fmt.Printf("公钥算法: %s\n", cert.PublicKeyAlgorithm)
-		fmt.Printf("签名: %x\n", cert.Signature)
-		fmt.Printf("是否是CA证书: %v\n", cert.IsCA)
-		fmt.Printf("最大路径长度: %d\n", cert.MaxPathLen)
-		fmt.Printf("主题密钥标识符: %x\n", cert.SubjectKeyId)
-		fmt.Printf("授权密钥标识符: %x\n", cert.AuthorityKeyId)
-		fmt.Printf("密钥用途: %v\n", cert.KeyUsage)
-		fmt.Printf("扩展密钥用途: %v\n", cert.ExtKeyUsage)
-		fmt.Printf("基本约束: %t\n", cert.BasicConstraintsValid)
-		fmt.Printf("DNS名称: %v\n", cert.DNSNames)
-		fmt.Printf("电子邮件地址: %v\n", cert.EmailAddresses)
-		fmt.Printf("IP地址: %v\n", cert.IPAddresses)
-		fmt.Printf("URI: %v\n", cert.URIs)
-		fmt.Printf("CRL分发点: %v\n", cert.CRLDistributionPoints)
-		fmt.Printf("OCSP服务器: %v\n", cert.OCSPServer)
-		fmt.Printf("证书策略: %v\n", cert.PolicyIdentifiers)
+		fmt.Println(cert.Subject)
+		allCNs[i] = cert.Subject.String()
+		allCNs[i+len(x509s)] = cert.Issuer.String()
 	}
+	allCNs = uniqueStrings(allCNs)
+	for _, cn := range allCNs {
+		var host string = strings.Split(cn, "=")[1]
+		var hosts []string = strings.Split(host, ",")
+		for _, host := range hosts {
+			_, err := url.Parse(host)
+			if err == nil {
+				endSub = cn
+				break
+			}
+		}
+		if len(endSub) > 0 {
+			break
+		}
+	}
+	fmt.Println("endSub=", endSub)
+	if len(endSub) == 0 {
+		endSub = allCNs[0]
+	}
+
+	for {
+		for i, cert := range x509s {
+			if cert.Subject.String() == endSub {
+				newx509s = append(newx509s, len(x509s)-i-1)
+				endSub = cert.Issuer.String()
+				break
+			}
+		}
+		if len(newx509s) == len(x509s) {
+			break
+		}
+	}
+	fmt.Println("newx509s=", newx509s)
+	for i, o := range newx509s {
+		var cert = x509s[o]
+		var subso = "└─"
+		if i == 0 {
+			subso = ""
+		}
+		fmt.Println(strings.Repeat("  ", i)+subso, cert.Subject)
+	}
+	order = newx509s
+}
+
+func viewCertInfo() bool {
+
+	// for i, cert := range x509s {
+	// 	fmt.Println("\n", lineStr, "证书", i+1, "/", len(x509s), lineStr)
+	// 	// fmt.Printf("版本: %d\n", cert.Version)
+	// 	// fmt.Printf("序列号: %d\n", cert.SerialNumber)
+	// 	// fmt.Printf("签名算法: %s\n", cert.SignatureAlgorithm)
+	// 	fmt.Printf("颁发者: %s\n", cert.Issuer)
+	// 	fmt.Printf("使用者: %s\n", cert.Subject)
+	// 	// fmt.Printf("有效期开始时间: %s\n", cert.NotBefore)
+	// 	// fmt.Printf("有效期结束时间: %s\n", cert.NotAfter)
+	// 	// fmt.Printf("公钥算法: %s\n", cert.PublicKeyAlgorithm)
+	// 	// fmt.Printf("签名: %x\n", cert.Signature)
+	// 	// fmt.Printf("是否是CA证书: %v\n", cert.IsCA)
+	// 	// fmt.Printf("最大路径长度: %d\n", cert.MaxPathLen)
+	// 	// fmt.Printf("主题密钥标识符: %x\n", cert.SubjectKeyId)
+	// 	// fmt.Printf("授权密钥标识符: %x\n", cert.AuthorityKeyId)
+	// 	// fmt.Printf("密钥用途: %v\n", cert.KeyUsage)
+	// 	// fmt.Printf("扩展密钥用途: %v\n", cert.ExtKeyUsage)
+	// 	// fmt.Printf("基本约束: %t\n", cert.BasicConstraintsValid)
+	// 	// fmt.Printf("DNS名称: %v\n", cert.DNSNames)
+	// 	// fmt.Printf("电子邮件地址: %v\n", cert.EmailAddresses)
+	// 	// fmt.Printf("IP地址: %v\n", cert.IPAddresses)
+	// 	// fmt.Printf("URI: %v\n", cert.URIs)
+	// 	// fmt.Printf("CRL分发点: %v\n", cert.CRLDistributionPoints)
+	// 	// fmt.Printf("OCSP服务器: %v\n", cert.OCSPServer)
+	// 	// fmt.Printf("证书策略: %v\n", cert.PolicyIdentifiers)
+	// }
 	return true
 }
